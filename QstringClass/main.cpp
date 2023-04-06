f802963f9268a5a449fdc0366203b93c3c0e2ae0
bsuman <tsui.champ@gmail.com>
2023-04-06 13:41:52 +0200

    QStringView Class Introduction
    QStringView Class usage, member functions

--- QstringClass/main.cpp
+++ QstringClass/main.cpp
@@ -1,13 +1,32 @@
 
 #include <QCoreApplication>
-#include<QString>
-#include<QStringList>
+#include <QString>
+#include <QStringList>
+#include <QStringView>
 
 /*
  * program to illustrate:
  * the use of Qstring with some of it's member functions.
- * the use of Qstringlist
+ *  Qstring is not a QObject.
+ *  It is does allow call by value and create copy of the input arguments
+ * the use of QStringlist
+ * the use of QStringView
+ * class to view the string without modifying the Qstring
+ * QStringView is faster because: it holds the pointer to the string and
+ * does not copy the string when used as argument.
+ *
 */
+
+
+void readname(QString name){
+    name.insert(0,"Ms."); // creates a copy of input string and modifies the copied string
+    qInfo()<< "Name inside readname function:"<<name;
+}
+
+void readonlyname(QStringView name){
+    //name.insert(0,"Ms."); // modifying the underlying string is not allowed
+    qInfo()<< "Name inside readonlyname function:"<<name.data();
+}
 
 int main(int argc, char *argv[])
 {
@@ -32,5 +51,10 @@ int main(int argc, char *argv[])
     foreach(QString s,ls){
         qInfo()<< "Split String:"<<s;
     }
+
+    QString name("Suman Bidarahalli");
+    readname(name);
+    qInfo()<< "Name:"<<name;
+
     return a.exec();
 }
